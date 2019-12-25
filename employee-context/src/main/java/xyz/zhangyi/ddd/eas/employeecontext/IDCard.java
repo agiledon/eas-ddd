e960291ff5cd9a738169c9458570ace76541a79f
agiledon <zhangyi1101@tom.com>
2019-12-25 09:40:46 +0800

    #2 - refactor validator for ID Card

--- employee-context/src/main/java/xyz/zhangyi/ddd/eas/employeecontext/IDCard.java
+++ employee-context/src/main/java/xyz/zhangyi/ddd/eas/employeecontext/IDCard.java
@@ -13,7 +13,9 @@ public class IDCard {
     private String number;
 
     public IDCard(String number) {
-        validateNumber(number);
+        IDCardValidator idCardValidator = new IDCardValidator(number);
+        idCardValidator.validate();
+        
         this.number = number;
     }
 
@@ -21,63 +23,72 @@ public class IDCard {
         return this.number;
     }
 
-    private void validateNumber(String number) {
-        validateNullOrEmptyNumber(number);
-        validateLengthOfNumber(number);
-        validateDigitNumbers(number);
-        validateBirthdayOfNumber(number);
-        validateChecksum(number);
-    }
+    public static class IDCardValidator {
+        static final int LENGTH_OF_IDCARD = 18;
+        private String number;
 
-    private void validateNullOrEmptyNumber(String number) {
-        if (Strings.isNullOrEmpty(number)) {
-            throw new InvalidIdCardException("Id card number should not be null or empty.");
+        IDCardValidator(String number) {
+            this.number = number;
         }
-    }
 
-    private void validateLengthOfNumber(String number) {
-        if (number.length() != 18) {
-            throw new InvalidIdCardException(String.format("Length of %s is not 18.", number));
+        private void validate() {
+            validateNullOrEmpty();
+            validateLength();
+            validateDigitalBits();
+            validateBirthday();
+            validateChecksum();
         }
-    }
 
-    private void validateDigitNumbers(String number) {
-        Pattern pattern = Pattern.compile("^[1-9]([0-9]{16})[xX0-9]$");
-        Matcher matcher = pattern.matcher(number);
-        if (!matcher.matches()) {
-            throw new InvalidIdCardException(String.format("%s is not begin with digit number.", number));
-        }
-    }
-
-    private void validateBirthdayOfNumber(String number) {
-        String birthdayPart = number.substring(6, 14);
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-        try {
-            LocalDate birthday = LocalDate.parse(birthdayPart, dateFormatter);
-            LocalDate minDate = LocalDate.of(1900, 1, 1);
-            LocalDate maxDate = LocalDate.now();
-            if (birthday.isBefore(minDate) || birthday.isAfter(maxDate)) {
-                throw new InvalidIdCardException(String.format("%s contains wrong birthday", number));
+        private void validateNullOrEmpty() {
+            if (Strings.isNullOrEmpty(this.number)) {
+                throw new InvalidIdCardException("Id card number should not be null or empty.");
             }
-        } catch (DateTimeParseException ex) {
-            throw new InvalidIdCardException(String.format("%s contains invalid birthday", number), ex);
         }
-    }
 
-    private void validateChecksum(String number) {
-        if (calculateChecksum(number) != number.charAt(17)) {
-            throw new InvalidIdCardException(String.format("Checksum of %s is wrong", number));
+        private void validateLength() {
+            if (this.number.length() != LENGTH_OF_IDCARD) {
+                throw new InvalidIdCardException(String.format("Length of %s is not 18.", this.number));
+            }
         }
-    }
 
-    private char calculateChecksum(String number) {
-        char[] checksum = {'1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'};
-        int[] weightOfChecksum = { 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
-        int sum = 0;
-        for (int i = 0; i < number.length() - 1; i++) {
-            char current = number.charAt(i);
-            sum += (current - '0') * weightOfChecksum[i];
+        private void validateDigitalBits() {
+            Pattern pattern = Pattern.compile("^[1-9]([0-9]{16})[xX0-9]$");
+            Matcher matcher = pattern.matcher(this.number);
+            if (!matcher.matches()) {
+                throw new InvalidIdCardException(String.format("%s is not begin with digit number.", this.number));
+            }
         }
-        return checksum[sum % 11];
+
+        private void validateBirthday() {
+            String birthdayPart = this.number.substring(6, 14);
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+            try {
+                LocalDate birthday = LocalDate.parse(birthdayPart, dateFormatter);
+                LocalDate minDate = LocalDate.of(1900, 1, 1);
+                LocalDate maxDate = LocalDate.now();
+                if (birthday.isBefore(minDate) || birthday.isAfter(maxDate)) {
+                    throw new InvalidIdCardException(String.format("%s contains wrong birthday", this.number));
+                }
+            } catch (DateTimeParseException ex) {
+                throw new InvalidIdCardException(String.format("%s contains invalid birthday", this.number), ex);
+            }
+        }
+
+        private void validateChecksum() {
+            if (calculateChecksum() != this.number.charAt(LENGTH_OF_IDCARD - 1)) {
+                throw new InvalidIdCardException(String.format("Checksum of %s is wrong", this.number));
+            }
+        }
+
+        private char calculateChecksum() {
+            char[] checksum = {'1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'};
+            int[] weightOfChecksum = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
+            int sum = 0;
+            for (int i = 0; i < this.number.length() - 1; i++) {
+                char current = this.number.charAt(i);
+                sum += (current - '0') * weightOfChecksum[i];
+            }
+            return checksum[sum % 11];
+        }
     }
 }
